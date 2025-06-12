73eb2e47ad4ddc5360f4038168c5ce802b8761d5
Edwin Rodriguez <chewinck7@gmail.com>
2025-06-12 18:01:56 -0500

    :wrench: Realizaando ajustes en docker compose local

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,3 +2,8 @@
 docker stop ceresyo-app-container
 docker stop ceresyo-server-container
 docker stop ceresyo-db-container
+
+
+docker rm ceresyo-app-container
+docker rm ceresyo-server-container
+docker rm ceresyo-db-container
