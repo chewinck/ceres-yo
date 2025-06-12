fb818e7bab34f1582b12e2b0a22117c42d124f22
Edwin Rodriguez <chewinck7@gmail.com>
2025-06-12 16:52:28 -0500

    :wrench: Agregar archivo de deploy en stage

---
+++ deployCeresyo.sh
@@ -0,0 +1,34 @@
+base_name="amy-ceresyo"
+container="ceresyo-app-container"
+image="1121872835/${base_name}"
+tag="qa"
+folder_env="ceresyo-env"
+compose_file="docker-compose-qa.yml"
+
+# Autenticarse en Docker Hub
+echo 'DockerHub/2024*' | docker login -u '1121872835' --password-stdin
+
+# Hacer pull de las imágenes definidas en docker-compose
+docker compose -f $compose_file pull
+
+# Detener y eliminar contenedores si ya existen
+echo "Deteniendo contenedores existentes (si los hay)..."
+docker compose -f $compose_file down
+
+# Reemplazar archivo .env si es necesario
+# Esto asume que estás copiando un nuevo archivo .env antes del deploy
+# cp ${HOME}/environments/${folder_env}/.env .env
+
+# Levantar todo con docker-compose
+echo "Levantando aplicación con docker-compose..."
+docker compose --env-file /home/adminqa/environments/ceresyo-env/.env -f $compose_file up -d
+
+
+# (Opcional) Limpiar imágenes antiguas si deseas
+oldImage=$(docker images $image | awk '{print $3}' | awk 'NR==2')
+if [ "$oldImage" ]; then
+    echo "Eliminando imagen antigua: $oldImage"
+    docker rmi $oldImage || true
+fi
+
+echo "🚀 Deploy finalizado con éxito."
